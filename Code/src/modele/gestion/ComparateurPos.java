5dce70c3751c5f0d155e415195e27710be8a1c55
Matthias BIZET <mabizet@IUTCLINFA2319.iut.local>
2022-01-11 09:52:56 +0100

    Modification ComparateurPosition

--- Code/src/modele/gestion/ComparateurPos.java
+++ Code/src/modele/gestion/ComparateurPos.java
@@ -7,7 +7,7 @@ import java.util.List;
 import static java.lang.Math.abs;
 
 public class ComparateurPos {
-    public Positions posPlusProche(Positions pos1, List<Positions> lesPos) {
+    public Positions posPlusProche(Positions posCj, List<Positions> lesPosCol, char d) {
         /*
         Le choix de la variable y1 ou x1 est arbitraire,
         l'algorithme fonctionne tant que l'on compare des "positions équivalentes" (x1 avec x1 ou x2 avec x2...)
@@ -18,20 +18,27 @@ public class ComparateurPos {
         Pour le second "if", on compare des valeurs absolues qui correspondent à l'écart entre la position actuelle et future
         du carré, il ya deux positions futures qui correspondent aux positions données par les deux types de collisionneur(ColMur et ColBtn)
 
-        Le but de la méthode est de rendre la position future la plus proche de la position actuelle (pour éviter de traverser des murs par exemple...)
+        Le but de la méthode est de rendre la plus proche des deux positions futures par rapport à la position actuelle
+        (pour éviter de traverser des murs par exemple...)
          */
-        if (pos1.getPosy1() == lesPos.get(0).getPosy1()) {
-            if(abs(pos1.getPosx1() - lesPos.get(0).getPosx1()) > abs(pos1.getPosx1() - lesPos.get(1).getPosx1())) {
-                return lesPos.get(1);
+        for(int i=0;i<lesPosCol.size()-1;i++) {
+            switch (d) {
+                case 'h' :
+
+                case 'b' :
+                    if (abs(posCj.getPosy1() - lesPosCol.get(i).getPosy1()) > abs(posCj.getPosy1() - lesPosCol.get(i+1).getPosy1()))
+                        return lesPosCol.get(i+1);
+                    else
+                        return lesPosCol.get(i);
+
+                case 'g' :
+
+                case 'd' :
+                    if(abs(posCj.getPosx1() - lesPosCol.get(i).getPosx1()) > abs(posCj.getPosx1() - lesPosCol.get(i+1).getPosx1()))
+                        return lesPosCol.get(i+1);
+                    else
+                        return lesPosCol.get(i);
             }
-            else
-                return lesPos.get(0);
-        }
-        else {
-            if (abs(pos1.getPosy1() - lesPos.get(0).getPosy1()) > abs(pos1.getPosy1() - lesPos.get(1).getPosy1())) {
-                return lesPos.get(1);
-            } else
-                return lesPos.get(0);
         }
     }
 }
