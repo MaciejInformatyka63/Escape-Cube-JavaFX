e5d61ef3b1af01b9ade6c0fc932874d7b065ccfb
Bastien MALVEZIN <bastien.malvezin@etu.uca.fr>
2022-01-20 18:01:10 +0100

    Ajout d'un deuxième niveau

--- Code/src/data/Stub.java
+++ Code/src/data/Stub.java
@@ -44,7 +44,7 @@ public class Stub implements Chargeur{
 
         n = new Niveau();
 
-        n.setCarreJoueur(new CarreJoueur(new Positions(51,101,51,101)));
+        n.setCarreJoueur(new CarreJoueur(new Positions(299,349,51,101)));
 
         n.addMur(new MurConcret(new Positions(0,50,0,400)));
         n.addMur(new MurConcret(new Positions(750,800,0,400)));
@@ -53,18 +53,20 @@ public class Stub implements Chargeur{
 
         n.addMur(new MurConcret(new Positions(50,148,201,249)));
         n.addMur(new MurConcret(new Positions(102,148,249,298)));
-        n.addMur(new MurConcret(new Positions(200,250,252,350)));
+        n.addMur(new MurConcret(new Positions(102,250,252,298)));
         n.addMur(new MurConcret(new Positions(102,400,102,149)));
         n.addMur(new MurConcret(new Positions(350,400,50,102)));
-        n.addMur(new MurConcret(new Positions(200,250,149,198)));
-        n.addMur(new MurConcret(new Positions(250,697,252,298)));
+        n.addMur(new MurConcret(new Positions(200,250,149,200)));
+        n.addMur(new MurConcret(new Positions(302,697,252,298)));
         n.addMur(new MurConcret(new Positions(452,500,102,252)));
         n.addMur(new MurConcret(new Positions(552,602,50,200)));
         n.addMur(new MurConcret(new Positions(654,697,102,252)));
 
         n.addBouton(new BoutonTimer(new Positions(51,101,250,300),false));
         n.addBouton(new BoutonTimer(new Positions(149,199,201,251),false));
-        n.addBouton(new BoutonTimer(new Positions(299,349,51,101),false));
+        n.addBouton(new BoutonTimer(new Positions(251,301,150,200),false));
+
+        //n.addBouton(new BoutonTimer(new Positions(299,349,51,101),false));
         n.addBouton(new BoutonTimer(new Positions(251,301,299,349),false));
 
         n.setSortie(new Sortie(new Positions(749,800,299,349)));
